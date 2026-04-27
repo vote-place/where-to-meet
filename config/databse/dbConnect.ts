e4cc7f3256236bb2a47ec24d61945e658a40af0c
toddla23 <qkrwhdgur0319@gmail.com>
2026-04-27 14:52:30 +0900

    [Config] add mysql2 package and create connection pool to mysql sever

---
+++ config/databse/dbConnect.ts
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+    port: parseInt(process.env.DB_PORT || "3306")
+});
+
+
+
+export default pool
