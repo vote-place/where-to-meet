912580e2608b3408b3f76a4f23f591775a35b925
toddla23 <qkrwhdgur0319@gmail.com>
2026-04-29 16:02:01 +0900

    [Add] add get all user in group api

--- app/api/groups/[code]/users/route.ts
+++ app/api/groups/[code]/users/route.ts
@@ -3,6 +3,17 @@ import groupService from "@/service/group";
 import userService from "@/service/user";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(req: NextRequest, { params }: { params: Promise<{ code: string }> }) {
+    const { code } = await params;
+
+    const group = await groupService.findByCode(code);
+    const users = await userService.findByGroup(group);
+
+    return NextResponse.json({ users })
+
+}
+
+
 export async function POST(req: NextRequest, { params }: { params: Promise<{ code: string }> }) {
     const body = await req.json();
     const { code } = await params;
